c706a7a800d125e3b82a8cb2fe0d948588256961
AlexLynx96 <nastyat1996@yandex.ru>
2021-10-16 16:57:06 +0300

    создание jar

---
+++ HW jar/src/mypkg/HelloWorld.java
@@ -0,0 +1,10 @@
+package mypkg;
+import mypkg2.TextMsg; 
+
+public class HelloWorld{
+
+    public static void main(String args[]){
+        System.out.println("Hello, World! - "+ TextMsg.msg);
+    }
+
+}
---
+++ HW jar/src/mypkg2/TextMsg.java
@@ -0,0 +1,7 @@
+package mypkg2;
+
+public class TextMsg{
+
+    public static String msg = "Привет, мир!";
+
+}
